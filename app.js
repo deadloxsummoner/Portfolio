8316a0cfc9812bc221524bd8e9d00636fd7e8aa9
PC1 <stan.magallon@gmail.com>
2024-10-20 00:05:06 +0800

    drop down

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let openNav = document.getElementById("openNav");
 let claseNav = document.getElementById("closeNav");
 let navBar = document.getElementById("navBar");
+let workOne = document.getElementById("workOne");
+
+
 
 function navOpen(){
     navBar.style.left = "0";
@@ -10,3 +13,8 @@ function navClose(){
     navBar.style.left ="100%";
 }
 
+function workOneOpen(){
+    workOne.classList.toggle("workClose");
+    workOne.classList.toggle("work-info-container");
+    
+}
